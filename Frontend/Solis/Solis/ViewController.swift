2731d8592687bb46dada2566a61d6113d2ba51ff
Julian <juwalu02@gmail.com>
2022-11-18 21:32:48 +0100

    Initial commit

---
+++ Frontend/Solis/Solis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Solis
+//
+//  Created by Julian Waluschyk on 18.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
